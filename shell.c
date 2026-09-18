8b87963c60dbf19cf362c6441db153bdf9c9c5f1
Your Name <you@example.com>
2026-09-17 20:41:19 -0400

    the main program for the shell

--- shell.c
+++ shell.c
@@ -4,18 +4,45 @@
  *
  * Return: Always 0
  */
-while (getline(...) != -1)
+int main(void)
 {
-	/* remove newline */
-	/* tokenize */
-	/* fork */
+	char **args;
+	char *input = NULL;
+	size_t buffer_size = 0;
+	int count = 0, mode = isatty(STDIN_FILENO), comparator;
 
-	if (fork() == 0)
+	while (1)
 	{
-		execve(args[0], args, environ);
-		perror(args[0]);
-		exit(127);
-	}
+		if (mode)
+			printf("#cisfun$ ");
+		if (getline(&input, &buffer_size, stdin) == -1) /* Reading the user input */
+		{
+			free(input);
+			break; /* Handling the eof (ctrl + D) */
+		}
+		count++;
+		if (input[strlen(input)] == '\n') /* Trim trailing the newline */
+			input[strlen(input)] = '\0';
+		args = parse_input(input); /* Tokenize the user input */
 
-	wait(...);
+		if (args == NULL)
+		{
+			free(input);
+			free(args);
+			continue;
+		}
+		if (args[0] != NULL)
+		{
+			comparator = env_fetch(args, input, count);
+			free(args);
+			if (comparator == 0)
+				continue;
+			else
+				break;
+		}
+		free(args);
+	}
+	if (mode)
+		printf("\n");
+	return (0);
 }
