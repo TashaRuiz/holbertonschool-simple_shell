b66b5c4326f31108b1e6bfc5be8c81dbd863fc3d
Your Name <you@example.com>
2026-09-18 07:20:58 -0400

    The main header for shell syntax

--- shell.h
+++ shell.h
@@ -10,10 +10,4 @@
 
 #define MAX_ARGS 10
 
-char *_getenv(const char *name);
-char **parse_input(char *input);
-int execute_command(char **args);
-int find_or_execute_command(char **args);
-int env_fetch(char **args, char *input, int count);
-extern char **environ;
 #endif
