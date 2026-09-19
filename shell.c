c7ddd55fbb254e9db0fc16467aed6d699facc834
Your Name <you@example.com>
2026-09-19 11:13:21 -0400

    the main program for the shell

--- shell.c
+++ shell.c
@@ -1,22 +1,159 @@
 #include "shell.h"
 /**
- * main - Simple UNIX command line interpreter
+ * get_path - gets PATH from environment
+ * @env: environment variables
  *
- * Return: Always 0
+ * Return: PATH value or NULL
+ */
+char *get_path(char **env)
+{
+	int i;
+
+	if (env == NULL)
+		return (NULL);
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		if (strncmp(env[i], "PATH=", 5) == 0)
+			return (env[i] + 5);
+	}
+	return (NULL);
+}
+/**
+ * find_command - finds a command in PATH
+ * @command: command to find
+ * @env: environment variables
+ *
+ * Return: full path to command or NULL
+ */
+char *find_command(char *command, char **env)
+{
+	char *path, *copy, *dir, *full;
+
+	if (command == NULL)
+		return (NULL);
+
+	if (strchr(command, '/'))
+	{
+		if (access(command, X_OK) == 0)
+			return (strdup(command));
+
+		return (NULL);
+	}
+
+	path = get_path(env);
+
+	if (path == NULL || *path == '\0')
+		return (NULL);
+
+	copy = strdup(path);
+	if (copy == NULL)
+		return (NULL);
+
+	dir = strtok(copy, ":");
+
+	while (dir != NULL)
+	{
+		full = build_path(dir, command);
+
+		if (full != NULL)
+		{
+			free(copy);
+			return (full);
+		}
+
+		dir = strtok(NULL, ":");
+	}
+	free(copy);
+	return (NULL);
+}
+/**
+ * execute_command - executes a command
+ * @args: command arguments
+ * @env: environment variables
+ * @program: program name
+ *
+ * Return: exit status of command
+ */
+int execute_command(char **args, char **env, char *program)
+{
+	char *command;
+	pid_t pid;
+	int status;
+
+	command = find_command(args[0], env);
+
+	if (command == NULL)
+	{
+		fprintf(stderr, "%s: 1: %s: not found\n",
+				program, args[0]);
+		return (127);
+	}
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+		free(command);
+		return (1);
+	}
+	if (pid == 0)
+	{
+		execve(command, args, env);
+		free(command);
+		exit(127);
+	}
+
+	waitpid(pid, &status, 0);
+	free(command);
+
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+
+	return (1);
+}
+/**
+ * process_line - processes one command line
+ * @line: command line
+ * @env: environment variables
+ * @program: program name
+ *
+ * Return: command status, or -1 to exit shell
+ */
+int process_line(char *line, char **env, char *program)
+{
+	char *args[64];
+
+	split_line(line, args);
+
+	if (args[0] == NULL)
+		return (0);
+
+	if (strcmp(args[0], "exit") == 0)
+		return (-1);
+
+	if (strcmp(args[0], "env") == 0)
+	{
+		print_env(env);
+		return (0);
+	}
+	return (execute_command(args, env, program));
+}
+/**
+ * main - Simple UNIX command line interpreter
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * @env: environment variables
+ *
+ * Return: status last command
  */
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	pid_t child;
-	int status;
-	/*char *argv[2];*/
-	char *argv[64];
-	char *token;
-	char *start;
-	char *end;
-	int argc = 0;
+	int status = 0;
+	int result;
 
 	while (1)
 	{
@@ -27,73 +164,16 @@ int main(void)
 	
 		if (read == -1)
 		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			/*free(line);*/
-			return (0);
+			break;
 		}
-
 		if (read > 0 && line[read - 1] == '\n')
 			line[read - 1] = '\0';
-		/*if(line[0] == '\0')
-			continue;
 
-		argv[0] = strtok(line, "\t");
-		argv[1] = NULL;
-	
-		if (argv[0] == NULL)
-			continue;*/
-		token = strtok(line, " \t");
-		while (token != NULL && argc < 63)
-		{
-			argv[argc] = token;
-			argc++;
-			token = strtok(NULL, " \t");
-		}
-		argv[argc] = NULL;
+		result = process_line(line, env, argv [0]);
 
-		if (argc == 0)
-			continue;
-
-		start = line;/*the begining of the program to avoid whitespace*/
-		while (*start == ' ' || *start == '\t')
-			start++;
-		end = start + strlen(start) - 1;
-		while (end >= start && (*end == ' ' || *end == '\t'))
-		{
-			*end = '\0';
-			end--;
-		}
-		argv[0] = start;
-		argv[1] = NULL;/*the end of whitespace program*/
-		
-		child = fork();
-		if (child == -1)
-		{
-			perror("./hsh");
-			free(line);
-			return (1);
-		}
-
-		if (child == 0)
-		{
-			execve(argv[0], argv, environ);
-			perror(argv[0]);
-			exit(1);
-		}
-		/*start = line;
-		while (*start == ' ' || *start == '\t')
-			start++;
-		
-		end = start + strlen(start) - 1;
-		while (end >= start && (*end == ' ' || *end == '\t'))
-		{
-			*end = '\0';
-			end--;
-		}
-		argv[0] = start;
-		argv[1] = NULL;*/
-		wait(&status);
+		if (result == -1)
+			break;
+		status = result;
 	}
 	free(line);
 	return (status);
