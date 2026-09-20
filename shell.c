3938d1ffd9fc189359488d796fd40bcb19203161
Your Name <you@example.com>
2026-09-20 07:15:09 -0400

    the main program for the shell

--- shell.c
+++ shell.c
@@ -149,7 +149,7 @@ int process_line(char *line, char **env, char *program)
  *
  * Return: status last command
  */
-int main( char **argv, char **env)
+int main(char **argv, char **env)
 {
 	char *line;
 	int status = 0;
