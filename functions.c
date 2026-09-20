b006921f89f6b8b669c5c50a4eb425cdf70bd085
Your Name <you@example.com>
2026-09-20 00:22:22 -0400

    base function that later be call in the shell.c

--- functions.c
+++ functions.c
@@ -57,3 +57,128 @@ void print_env(char **env)
 		write(STDOUT_FILENO, "\n", 1);
 	}
 }
+/**
+ * string_duplicate - duplicates a string
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string, or NULL
+ */
+char *string_duplicate(char *str)
+{
+	char *copy;
+	int i;
+	int length;
+
+	if (str == NULL)
+		return (NULL);
+
+	length = 0;
+	while (str[length] != '\0')
+		length++;
+
+	copy = malloc(sizeof(char) * (length + 1));
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i <= length; i++)
+		copy[i] = str[i];
+
+	return (copy);
+}
+/**
+ * find_character - finds a character in a string
+ * @str: string to search
+ * @character: character to find
+ *
+ * Return: pointer to character, or NULL
+ */
+char *find_character(char *str, char character)
+{
+	if (str == NULL)
+		return (NULL);
+
+	while (*str != '\0')
+	{
+		if (*str == character)
+			return (str);
+
+		str++;
+	}
+
+	return (NULL);
+}/**
+ * string_compare - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if strings are equal
+ */
+int string_compare(char *s1, char *s2)
+{
+	int i;
+
+	if (s1 == NULL || s2 == NULL)
+		return (-1);
+
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
+/**
+ * read_line - reads a line from standard input
+ *
+ * Return: allocated line, or NULL on EOF/error
+ */
+char *read_line(void)
+{
+	char *line;
+	char character;
+	int i;
+	ssize_t bytes;
+
+	line = malloc(1024);
+	if (line == NULL)
+		return (NULL);
+
+	i = 0;
+
+	while (i < 1023)
+	{
+		bytes = read(STDIN_FILENO, &character, 1);
+
+		if (bytes == 0)
+		{
+			if (i == 0)
+			{
+				free(line);
+				return (NULL);
+			}
+
+			break;
+		}
+
+		if (bytes == -1)
+		{
+			free(line);
+			return (NULL);
+		}
+
+		if (character == '\n')
+			break;
+
+		line[i] = character;
+		i++;
+	}
+
+	line[i] = '\0';
+
+	return (line);
+}
