c1b774ac9d0889055ceed2c2d5681db3a0d54072
Your Name <you@example.com>
2026-09-20 10:13:55 -0400

    the main program for the shell

--- shell.c
+++ shell.c
@@ -183,7 +183,7 @@ int main(int argc, char **argv, char **env)
 			break;
 		}
 
-		result = process_line(line, env, argv[0]);
+		result = process_line(line, env, argv[0], &exit_shell);
 		free(line);
 
 		status = result;
