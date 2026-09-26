e8d5ff022ac43b0970ab9a909b4b20d6031409a0
Your Name <you@example.com>
2026-09-26 11:25:58 -0400

    The main header for shell syntax

--- shell.h
+++ shell.h
@@ -32,4 +32,5 @@ int add_environment(char *variable, char ***env);
 int handle_unsetenv(char **args, char ***env);
 int handle_cd(char **args, char ***env);
 int update_directory_vars(char ***env, char *oldpwd, char *newpwd);
+int execute_one_command(char *line, char ***env, char *program, int *exit_shell, int last_status);
 #endif
