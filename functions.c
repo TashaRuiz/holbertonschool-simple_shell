8f301682fe0603d1ff2a7c43e8191093ba53cbfe
Your Name <you@example.com>
2026-09-26 09:56:48 -0400

    base function that later be call in the shell.c

--- functions.c
+++ functions.c
@@ -8,42 +8,6 @@
  * Return: 0 on success, 1 on failure
  */
 int update_directory_vars(char ***env, char *oldpwd, char *newpwd)
-	int old_len;
-	int new_len;
-	char *new_value;
-
-	if (oldpwd == NULL || newpwd == NULL)
-		return (1);
-
-	old_len = string_length(oldpwd);
-	new_len = string_length(newpwd);
-
-	for (i = 0; env[i] != NULL; i++)
-	{
-		if (string_starts_with(env[i], "OLDPWD="))
-		{
-			new_value = malloc(old_len + 8);
-
-			if (new_value == NULL)
-				return (1);
-
-			sprintf(new_value, "OLDPWD=%s", oldpwd);
-			free(env[i]);
-			env[i] = new_value;
-		}
-		else if (string_starts_with(env[i], "PWD="))
-		{
-			new_value = malloc(new_len + 5);
-
-			if (new_value == NULL)
-				return (1);
-
-			sprintf(new_value, "PWD=%s", newpwd);
-			free(env[i]);
-			env[i] = new_value;
-		}
-	}
-	return (0);env, char *oldpwd, char *newpwd)
 {
 	int i;
 	int old_len;
